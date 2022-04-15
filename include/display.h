c23b5629d40a3629263905b78de20f3fd47b6df7
Tobi Wulff <tobi@swulff.de>
2022-04-16 11:20:55 +1200

    Change display to SSD1306

--- include/display.h
+++ include/display.h
@@ -2,9 +2,8 @@
 #define __DISPLAY_H__
 
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
+#include <Adafruit_SSD1306.h>
 
 class Display {
     public:
@@ -12,9 +11,9 @@ class Display {
         void init();
         void clear();
         void print_text(int x, int y, const char *s);
-        void draw_rect(int x, int y, int w, int h, int color);
+        void draw_rect(int x, int y, int w, int h);
     private:
-        Adafruit_ST7735 *tft;
+        Adafruit_SSD1306 *tft;
 };
 
 #endif
--- src/display.cpp
+++ src/display.cpp
@@ -1,42 +1,35 @@
 #include "display.h"
 
-#define PIN_TFT_CLK 15
-#define PIN_TFT_SDA 16
-#define PIN_TFT_RES 7
-#define PIN_TFT_DC 8
-#define PIN_TFT_CS 9
-
 Display::Display()
 {
 }
 
 void Display::init()
 {
-    tft = new Adafruit_ST7735(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_SDA, PIN_TFT_CLK, PIN_TFT_RES);
+    tft = new Adafruit_SSD1306(128, 32, &Wire, -1);
 
-    // Need to modify Adafruit_ST7735.cpp: remove MADCTL_MX to de-mirror text
-    tft->initR(INITR_MINI160x80);
-    tft->setRotation(3);
-    tft->invertDisplay(true); // required
-    tft->fillScreen(0);
+    tft->begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
-    tft->setTextSize(2);
-    tft->setTextColor(ST77XX_WHITE);
+    tft->display();
+
+    tft->setTextSize(1);
+    tft->setTextColor(SSD1306_WHITE);
     tft->setTextWrap(false);
 }
 
 void Display::clear()
 {
-    tft->fillScreen(0);
+    tft->clearDisplay();
 }
 
 void Display::print_text(int x, int y, const char *s)
 {
     tft->setCursor(x, y);
     tft->print(s);
+    tft->display();
 }
 
-void Display::draw_rect(int x, int y, int w, int h, int color)
+void Display::draw_rect(int x, int y, int w, int h)
 {
-    tft->fillRect(x, y, w, h, color);
+    tft->fillRect(x, y, w, h, SSD1306_WHITE);
 }
