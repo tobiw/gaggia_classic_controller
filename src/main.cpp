999e2bd6ec5a8c79e81feeb5350ae0fe2ed19d7d
Tobi Wulff <tobi@swulff.de>
2022-04-16 11:21:15 +1200

    Working in manual mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,17 @@
 #include "heater_controller.h"
 #include "display.h"
 
-#define PIN_BTN1 A3
-#define PIN_SSR A2
-#define PIN_STATUS_INDICATOR 13 // built-in LED
+#define PIN_SDA 2
+#define PIN_SCL 3
+#define PIN_BTN1 4
+#define PIN_BTN2 7
+#define PIN_SSR 5
 #define PIN_ADC_PRESSURE A0
+#define PIN_LED_R 19
+#define PIN_LED_G 20
+#define PIN_LED_B 21
+
+#define DISPLAY_UPDATE_INTERVAL 500
 
 enum heating_status_t {
     STATUS_COLD = 0,
@@ -24,90 +31,119 @@ HeaterController heater_controller;
 
 Display display;
 
-volatile int last_btn = HIGH;
-volatile bool btn1_pressed = false;
-volatile unsigned long last_btn_press_time = 0;
+int last_btn = HIGH;
+unsigned long last_btn_press_time = 0;
 
-// Note: cannot use delay() in ISR for debouncing
-void isr_btn1() {
-    const int btn = digitalRead(PIN_BTN1);
-    if (!btn1_pressed && digitalRead(PIN_BTN1) == LOW && last_btn == HIGH) {
-        btn1_pressed = true;
-        last_btn_press_time = millis();
+void set_rgb_led(uint8_t r, uint8_t g, uint8_t b) {
+    if (r + g + b <= 1) {
+        digitalWrite(PIN_LED_R, r == 1 ? LOW : HIGH);
+        digitalWrite(PIN_LED_G, g == 1 ? LOW : HIGH);
+        digitalWrite(PIN_LED_B, b == 1 ? LOW : HIGH);
+    } else { // flash to mix multiple colours
+        // TODO: non-blocking
     }
-    last_btn = btn;
 }
 
 void setup() {
     Serial.begin(115200);
     display.init();
+    display.clear();
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+    // RGB LEDs
+#define LED_TEST_DELAY 200
+    pinMode(PIN_LED_R, OUTPUT);
+    pinMode(PIN_LED_G, OUTPUT);
+    pinMode(PIN_LED_B, OUTPUT);
+    set_rgb_led(1, 0, 0);
+    delay(LED_TEST_DELAY);
+    set_rgb_led(0, 1, 0);
+    delay(LED_TEST_DELAY);
+    set_rgb_led(0, 0, 1);
+    delay(LED_TEST_DELAY);
+    set_rgb_led(0, 0, 0);
+    delay(LED_TEST_DELAY);
 
     pinMode(PIN_ADC_PRESSURE, INPUT);
 
     pinMode(PIN_SSR, OUTPUT);
     digitalWrite(PIN_SSR, LOW);
     pinMode(PIN_BTN1, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(PIN_BTN1), isr_btn1, FALLING);
+    pinMode(PIN_BTN2, INPUT_PULLUP);
 
     heater_action = HEATER_OFF;
     heater_controller.set_target(80);
 }
 
 void display_set_status_color(heating_status_t s) {
-    switch (s) {
+    const int y = 24;
+    // TODO: based on HeaterController status instead of user input
+    /*switch (s) {
         case STATUS_COLD:
-            display.draw_rect(10, 40, 100, 70, ST77XX_BLUE);
+            display.print_text(0, y, "Cold");
+            set_rgb_led(0, 0, 1);
             break;
         case STATUS_HEATING:
-            display.draw_rect(10, 40, 100, 70, ST77XX_RED);
+            display.print_text(0, y, "Heating");
+            set_rgb_led(1, 0, 0);
             break;
         case STATUS_NEAR_TARGET:
-            display.draw_rect(10, 40, 100, 70, ST77XX_ORANGE);
+            display.print_text(0, y, "Near");
+            set_rgb_led(1, 0, 0);
             break;
         case STATUS_HOT:
-            display.draw_rect(10, 40, 100, 70, ST77XX_GREEN);
+            display.print_text(0, y, "Hot");
+            set_rgb_led(0, 1, 0);
             break;
-    }
+    }*/
+    set_rgb_led(heater_action == HEATER_ON ? 1 : 0, 0, 0);
 }
 
 void set_ssr(heater_action_t a) {
     if (a == HEATER_ON) {
         digitalWrite(PIN_SSR, HIGH);
-        //digitalWrite(PIN_STATUS_INDICATOR, HIGH);
+        set_rgb_led(1, 0, 0);
     } else {
         digitalWrite(PIN_SSR, LOW);
-        //digitalWrite(PIN_STATUS_INDICATOR, LOW);
+        set_rgb_led(0, 0, 0);
     }
 }
 
 void loop() {
-    static int t = 60;
+    static int t = 20;
     static char buf[32];
-    const unsigned int t_target = 80;
     static heater_action_t heater_action = HEATER_OFF;
+    static heater_action_t last_heater_action = HEATER_OFF;
     static heating_status_t heating_status = STATUS_COLD;
+    const unsigned int t_target = 80;
     const unsigned int pressure_raw = analogRead(PIN_ADC_PRESSURE);
 
     static unsigned long last_update_time = 0;
     const unsigned long m = millis();
 
     // Handle button press -> turn heating on or off
-    if (btn1_pressed && ((last_btn_press_time + 200) < m)) {
-        heater_action = (heater_action == HEATER_OFF) ? HEATER_ON : HEATER_OFF;
-        btn1_pressed = false;
+    if (digitalRead(PIN_BTN1) == LOW && last_btn == HIGH) { // pressed
+        if ((last_btn_press_time + 200) < m) { // debounce
+            heater_action = (heater_action == HEATER_OFF) ? HEATER_ON : HEATER_OFF;
+            last_btn_press_time = m;
+            last_btn = LOW;
+        }
+    } else if (digitalRead(PIN_BTN1) == HIGH && last_btn == LOW) { // released
+        last_btn = HIGH;
     }
 
     // Set SSR based on HeaterController, not direct from button inputs or measured temperature
-    heater_controller.update(t);
-    if (heater_action == HEATER_ON && heater_controller.get_output() == 1) set_ssr(HEATER_ON);
-    else set_ssr(HEATER_OFF);
+    // TODO: Use HeaterController (PID)
+    //heater_controller.update(t);
+    //if (heater_action == HEATER_ON && heater_controller.get_output() == 1) set_ssr(HEATER_ON);
+    if (heater_action != last_heater_action) {
+        if (heater_action == HEATER_ON) set_ssr(HEATER_ON);
+        else set_ssr(HEATER_OFF);
+        last_heater_action = heater_action;
+    }
 
     // Only update Serial and display once per second
-    if ((last_update_time + 1000) < m) {
-        if (t < (t_target - 5)) {
+    if ((last_update_time + DISPLAY_UPDATE_INTERVAL) < m) {
+        /*if (t < (t_target - 5)) {
             if (heater_action == HEATER_ON) {
                 t += 2; // rapid increase during heating phase
                 heating_status = STATUS_HEATING;
@@ -125,14 +161,13 @@ void loop() {
             heating_status = STATUS_HOT;
         } else {
             heating_status = STATUS_COLD;
-        }
+        }*/
 
         // Pressure conversion
         // Max pressure of sensor is 1.2 Mpa = 174 psi = 12 bar
-        // voltage range of sensor is 4.5V
-        // raw value is 0-4096 with Vref 1.1V?
-        double pressure_V = pressure_raw * (3.3 / 4096.0); // factor is (maxV / maxDigitalValue)
-        double pressure_bar = ((pressure_V - (480 * 3.3 / 4096.0)) * 3.0); // subtract 1 bar pressure voltage and apply conversion factor
+        // voltage range of sensor is 0.5-4.5V = 4V
+        double pressure_V = pressure_raw * (5.0 / 1024.0); // factor is (maxV / maxDigitalValue)
+        double pressure_bar = (pressure_V - 0.17) * 3.0; // subtract 1 bar pressure voltage and apply conversion factor
 
         Serial.print("LOOP: status ");
         Serial.print(heating_status);
@@ -146,15 +181,15 @@ void loop() {
         Serial.println(pressure_bar);
 
         display.clear();
-        display_set_status_color(heating_status);
-        display.print_text(10, 10, "T:");
+        //display_set_status_color(heating_status);
+        display.print_text(0, 0, "T:");
         dtostrf((double)t, 3, 1, buf);
-        display.print_text(40, 10, buf);
+        display.print_text(20, 0, buf);
         sprintf(buf, "(%u)", t_target);
-        display.print_text(90, 10, buf);
-        display.print_text(10, 20, heater_action == HEATER_OFF ? "OFF" : "ON");
-        sprintf(buf, "P: %u", pressure_raw);
-        display.print_text(80, 64, buf);
+        display.print_text(50, 0, buf);
+        display.print_text(50, 20, heater_action == HEATER_OFF ? "OFF" : "ON");
+        dtostrf(pressure_bar, 2, 1, buf);
+        display.print_text(80, 20, buf);
 
         last_update_time = m;
     }
